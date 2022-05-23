6409d3130c535a2abc5a7238c81573ac5aafa8ca
valeria-ab <lera.magnum@gmail.com>
2022-05-23 19:05:01 +0300

    adaptability

--- src/components/PhotosBlock/GalleryBlock.tsx
+++ src/components/PhotosBlock/GalleryBlock.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PhotoItem from './PhotoItem/PhotoItem';
 import {PaintingsResponseType} from '../../utils/api';
+import s from './Gallery.module.css';
 import {useSelector} from 'react-redux';
 import {IAppStore} from '../../store/store';
 
@@ -9,11 +10,14 @@ const GalleryBlock = () => {
     const currentPage = useSelector<IAppStore, number>(state => state.gallery.currentPage)
     const itemsPerPage = useSelector<IAppStore, number>(state => state.gallery.itemsPerPage)
     const paintings = useSelector<IAppStore, Array<PaintingsResponseType>>(state => state.gallery.paintings)
+    const isNightModeOn = useSelector<IAppStore, boolean>(state => state.gallery.isNightModeOn)
 
+    const notFoundTextSpan = isNightModeOn
+        ? {color: 'white', margin: "20px auto"}
+        : {color: 'black', margin: "20px auto"}
     let end = currentPage * itemsPerPage
     let start = end - itemsPerPage
 
-    // let paintings
     let mappedArray = paintings.slice(start, end);
     if (paintings.length < 12) {
         mappedArray = paintings
@@ -21,13 +25,10 @@ const GalleryBlock = () => {
 
 
     return (
-        <div style={{
-            display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between',
-            minHeight: '56vh',
-        }}>
+        <div className={s.gallery__container}>
             {paintings.length
                 ? mappedArray.map(p => <PhotoItem key={p.id} picture={p}/>)
-                : <div style={{color: 'black', margin: "20px auto"}}>There are no pictures matching the specified filter.
+                : <div style={notFoundTextSpan}>There are no pictures matching the specified filter.
                     Please set a different value.</div>
             }
         </div>
